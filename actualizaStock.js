99b76b70a8a22d64d376f6e1c39dd867e9ff03d9
unknown <manuelcursowebutrera@gmail.com>
2019-12-16 09:41:52 +0100

    Segunda Versión: Proyecto Gestión Almacén

--- actualizaStock.js
+++ actualizaStock.js
@@ -1,10 +1,13 @@
 function actualizarStock() {
-    var cantidad=document.getElementById('numPro').value;
+	var cantidad= parseInt(document.getElementById('numPro').value);
+	var stock;
 
     if (cantidad == "" || cantidad == 0) {
         document.getElementById('resultado').value = "Necesita indicar una cantidad del producto";
 	}
 	else {
+		stock=cantidad;
 		document.getElementById('resultado').value= "Se han ingresado " + cantidad + " unidades del producto";
+		document.getElementById('stock').value=stock;
 	}
 }
